50ba9df9d355829e1b992196d5e8169f76a9a4a9
Nikolai Podkorytov <nikopodk@gmail.com>
2025-05-15 18:49:30 +0300

    Removed embedded Git repo in frontend

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+ 
 // Routes
 app.use('/api/movies', require('./routes/movies'));
 app.use('/api/reviews', require('./routes/reviews'));
